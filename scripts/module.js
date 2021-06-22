3c5a6b3d58e98f9a519a06b0272886c376db045f
Matthew Kendon <matt@outlandish.com>
2021-06-22 21:25:16 +0100

    add hooks to allow items  to have slots

--- scripts/module.js
+++ scripts/module.js
@@ -48,7 +48,72 @@ Hooks.once('ready', async function() {
         } catch (e) {console.error(e);}
 
     }, 'WRAPPER');
+
+    console.log("Patching CONFIG.Actor.entityClass.prototype._computeEncumbrance");
+    libWrapper.register(MODULE_NAME, 'CONFIG.Actor.entityClass.prototype._computeEncumbrance', function (actorData) {
+        // Get the total weight from items
+        const physicalItems = ["weapon", "equipment", "consumable", "tool", "backpack", "loot"];
+        let bulk = actorData.items.reduce((bulk, i) => {
+            if ( !physicalItems.includes(i.type) ) return bulk;
+            const q = i.data.quantity || 0;
+            const w = i.data.slots || 0;
+            return bulk + (q * w);
+        }, 0);
+
+        // [Optional] add Currency Weight (for non-transformed actors)
+        if ( game.settings.get("dnd5e", "currencyWeight") && actorData.data.currency ) {
+            const currency = actorData.data.currency;
+            const numCoins = Object.values(currency).reduce((val, denom) => val += Math.max(denom, 0), 0);
+            bulk += numCoins / CONFIG.DND5E.encumbrance.currencyPerWeight;
+        }
+
+        // Determine the encumbrance size class
+        let mod = {
+            tiny: 0.5,
+            sm: 1,
+            med: 1,
+            lg: 2,
+            huge: 4,
+            grg: 8
+        }[actorData.data.traits.size] || 1;
+        if ( this.getFlag("dnd5e", "powerfulBuild") ) mod = Math.min(mod * 2, 8);
+
+        // Compute Encumbrance percentage
+        bulk = bulk.toNearest(0.1);
+        const max = actorData.data.abilities.str.value * CONFIG.DND5E.encumbrance.strMultiplier * mod;
+        const pct = Math.clamped((bulk * 100) / max, 0, 100);
+        return { value: bulk.toNearest(0.1), max, pct, encumbered: pct > (2/3) };
+
+    }, 'OVERRIDE');
+
+    Hooks.on("renderItemSheet", function(app, html, data) {
+        let weight = html.find(`.item-properties .form-group input[name='data.weight']`)[0].parentElement;
+
+        if (weight) {
+            renderItemSlotsField(weight, data)
+        }
+
+    });
+
+    //Slots won't get carried over, so this gets the slots value
+    //from the preCreateOwnedItem and then added a one off hook
+    //to assign the slots value back to the item after it has
+    //been stripped out.
+    Hooks.on("preCreateOwnedItem", function (entity, data) {
+        let slotsValue = data.data.slots
+
+        Hooks.once("createOwnedItem", function (entity, data) {
+            data.data.slots = slotsValue
+        })
+
+    })
+
 });
+
+async function renderItemSlotsField(weight,  data) {
+    let el = await renderTemplate('modules/5e-darker-dungeons/templates/item-slots-field.html', data)
+    $(weight).after(el)
+}
 
 Hooks.on("applyActiveEffect", monsterMakerApplyAC)
 Hooks.on("applyActiveEffect", monsterMakerApplyAbilities)
