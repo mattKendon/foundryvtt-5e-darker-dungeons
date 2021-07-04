eaca2297f59fc471c92ecee47685625b22b0784a
Matthew Kendon <matt@outlandish.com>
2021-07-04 16:37:19 +0100

    change try catch

--- scripts/active-inventory/index.js
+++ scripts/active-inventory/index.js
@@ -34,10 +34,11 @@ export function computeEncumbrance(actorData) {
         if ( !physicalItems.includes(i.type) ) return bulk;
         if (i.type === 'equipment' && attireItems.includes(i.data.armor.type) && i.data.equipped) return bulk;
         const q = i.data.quantity || 0;
-        let w = 0
+        let w;
         try {
             w = i.flags[configuration.MODULE_NAME].slots || 0;
         } catch {
+            w = 0
         }
         return bulk + (q * w);
     }, 0);
