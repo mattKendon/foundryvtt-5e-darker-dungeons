a7777e1b7ea164ea8e74f2ae64009caada71ff0b
Matthew Kendon <matt@outlandish.com>
2021-07-08 23:04:14 +0100

    have game setting to turn on active inventory

--- scripts/module.js
+++ scripts/module.js
@@ -31,6 +31,14 @@ Hooks.once('init', async function() {
         default: false,
         type: Boolean,
     });
+    game.settings.register(configuration.MODULE_NAME, "active-inventory", {
+        name: 'Active Inventory',
+        hint: 'Use slots to calculate inventory capacity and encumberance',
+        scope: 'world',
+        config: true,
+        default: false,
+        type: Boolean,
+    });
 });
 
 function addSlotsToItems(wrapper, data) {
@@ -70,53 +78,56 @@ Hooks.once('ready', async function() {
 
     }, 'WRAPPER');
 
-    console.log("Patching CONFIG.Actor.entityClass.prototype._computeEncumbrance");
-    libWrapper.register(configuration.MODULE_NAME, 'CONFIG.Actor.entityClass.prototype._computeEncumbrance', computeEncumbrance, 'OVERRIDE');
+    if (game.settings.get(configuration.MODULE_NAME, "active-inventory")) {
 
-    console.log("Patching game.dnd5e.applications.ActorSheet5eCharacter.prototype._prepareItems");
-    libWrapper.register(configuration.MODULE_NAME, 'game.dnd5e.applications.ActorSheet5eCharacter.prototype._prepareItems', addSlotsToItems, 'WRAPPER');
+        console.log("Patching CONFIG.Actor.entityClass.prototype._computeEncumbrance");
+        libWrapper.register(configuration.MODULE_NAME, 'CONFIG.Actor.entityClass.prototype._computeEncumbrance', computeEncumbrance, 'OVERRIDE');
 
-    Hooks.on("renderItemSheet", function(app, html, data) {
-        data.data.slots = app.object.getFlag(configuration.MODULE_NAME, 'slots')
-        let weight = html.find(`.item-properties .form-group input[name='data.weight']`)[0].parentElement;
+        console.log("Patching game.dnd5e.applications.ActorSheet5eCharacter.prototype._prepareItems");
+        libWrapper.register(configuration.MODULE_NAME, 'game.dnd5e.applications.ActorSheet5eCharacter.prototype._prepareItems', addSlotsToItems, 'WRAPPER');
 
-        if (weight) {
-            renderItemSlotsField(weight, data)
-        }
+        Hooks.on("renderItemSheet", function(app, html, data) {
+            data.data.slots = app.object.getFlag(configuration.MODULE_NAME, 'slots')
+            let weight = html.find(`.item-properties .form-group input[name='data.weight']`)[0].parentElement;
 
-    });
+            if (weight) {
+                renderItemSlotsField(weight, data)
+            }
 
-    Hooks.on("renderActorSheet5eCharacter", function(app, html) {
-        html.find(`.item-detail.item-weight .item-detail`).each(function () {
-            this.innerText = this.innerText.replace("lbs.", "")
+        });
+
+        Hooks.on("renderActorSheet5eCharacter", function(app, html) {
+            html.find(`.item-detail.item-weight .item-detail`).each(function () {
+                this.innerText = this.innerText.replace("lbs.", "")
+            })
+            html.find(`.items-header .item-detail.item-weight`).each(function () {
+                this.innerText = "Slots"
+            })
+        });
+
+        Hooks.on("updateItem", function (entity, data) {
+            if (data.hasOwnProperty('data') && data.data.hasOwnProperty('slots')) {
+                entity.setFlag(configuration.MODULE_NAME, 'slots', data.data.slots)
+            }
         })
-        html.find(`.items-header .item-detail.item-weight`).each(function () {
-            this.innerText = "Slots"
+        Hooks.on("updateOwnedItem", function (entity, data) {
+            if (data.hasOwnProperty('data') && data.data.hasOwnProperty('slots')) {
+                entity.items.filter((i)=> i.data._id == data._id)[0].setFlag(configuration.MODULE_NAME, 'slots', data.data.slots)
+            }
         })
-    });
 
-    Hooks.on("updateItem", function (entity, data) {
-        if (data.hasOwnProperty('data') && data.data.hasOwnProperty('slots')) {
-            entity.setFlag(configuration.MODULE_NAME, 'slots', data.data.slots)
-        }
-    })
-    Hooks.on("updateOwnedItem", function (entity, data) {
-        if (data.hasOwnProperty('data') && data.data.hasOwnProperty('slots')) {
-            entity.items.filter((i)=> i.data._id == data._id)[0].setFlag(configuration.MODULE_NAME, 'slots', data.data.slots)
-        }
-    })
+        //Slots won't get carried over, so this gets the slots value
+        //from the preCreateOwnedItem and then added a one off hook
+        //to assign the slots value back to the item after it has
+        //been stripped out.
 
-    //Slots won't get carried over, so this gets the slots value
-    //from the preCreateOwnedItem and then added a one off hook
-    //to assign the slots value back to the item after it has
-    //been stripped out.
-
-    Hooks.on("createOwnedItem", function (entity, data) {
-        data.data.slots = entity.items.filter((i)=> i.data._id == data._id)[0].getFlag(configuration.MODULE_NAME, 'slots')
-    })
-    Hooks.once("createItem", function (entity) {
-        entity.data.data.slots = entity.getFlag(configuration.MODULE_NAME, 'slots')
-    })
+        Hooks.on("createOwnedItem", function (entity, data) {
+            data.data.slots = entity.items.filter((i)=> i.data._id == data._id)[0].getFlag(configuration.MODULE_NAME, 'slots')
+        })
+        Hooks.once("createItem", function (entity) {
+            entity.data.data.slots = entity.getFlag(configuration.MODULE_NAME, 'slots')
+        })
+    }
 
 });
 
